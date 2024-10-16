6700165ba15c12ee63c1d302c6829d413f2b581e
ririsutri <sutrikuliah@gmail.com>
2024-10-16 11:04:34 +0700

    Added Checkin Logic Now Can Upload Photo for WFH

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const cookieParser = require("cookie-parser");
 const { verifyToken, checkRole } = require("./middleware/authMiddleWare.js");
 const { infinite_track_connection: db } = require("./dbconfig.js");
 const sendOTP = require("./utils/nodeMailer");
+const multer = require("multer");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -29,13 +31,33 @@ function generateOTP() {
 
 // Fungsi untuk mendapatkan ID kategori absensi berdasarkan nama
 const getAttendanceCategoryId = (category) => {
-  return category === "wfh" ? 1 : 2; // 1 untuk WFH, 2 untuk WFO
+  return category === "Work From Office" ? 1 : 2; // 1 untuk WFH, 2 untuk WFO
 };
 
 // Fungsi untuk mendapatkan ID status absensi
 const getAttendanceStatusId = (status) => {
   return status === "late" ? 1 : 2; // 1 untuk Late, 2 untuk Confirm
 };
+
+// Set storage for Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/"); // Menentukan direktori tempat menyimpan file
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    ); // Menentukan nama file
+  },
+});
+
+// Set upload middleware
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // Batas ukuran file 5MB
+});
 
 // Endpoint untuk mengirim OTP
 app.post("/send-otp", async (req, res) => {
@@ -539,50 +561,76 @@ app.get("/management", verifyToken, checkRole(["Management"]), (req, res) => {
 });
 
 // Endpoint untuk Check-In
-app.post("/attendance/checkin", verifyToken, (req, res) => {
-  const { attendance_category } = req.body;
-  const attendance_category_id = getAttendanceCategoryId(attendance_category);
-  const userId = req.user.id;
+app.post(
+  "/attendance/checkin",
+  verifyToken,
+  upload.single("upload_image"),
+  (req, res) => {
+    const { attendance_category } = req.body;
+    const attendance_category_id = getAttendanceCategoryId(attendance_category);
+    const userId = req.user.id;
 
-  const now = new Date();
-  const currentHour = now.getHours();
+    const now = new Date();
+    const currentHour = now.getHours();
 
-  let attendance_status_id = currentHour < 9 ? 1 : 2;
+    let attendance_status_id = currentHour < 9 ? 1 : 2;
+    let upload_image = null;
 
-  db.query(
-    "INSERT INTO attendance (check_in_time, check_out_time,  userId, attendance_category_id, attendance_status_id, attendance_date) VALUES (NOW(), NULL, ?, ?, ?, CURDATE())",
-    [userId, attendance_category_id, attendance_status_id],
-    (err, result) => {
-      if (err) {
-        console.error("Error during check-in:", err.message);
-        return res.status(500).json({ message: "Failed to check in" });
+    // Cek apakah user WFH (attendance_category_id = 2) dan mengupload gambar
+    if (attendance_category_id === 2) {
+      if (!req.file) {
+        return res
+          .status(400)
+          .json({ message: "Image is required for Work From Home" });
       }
+      upload_image = req.file.path; // Path gambar yang diupload
+    } else {
+      // Untuk WFO, kirimkan string kosong atau nilai default jika tidak ada gambar
+      upload_image = ""; // Atau bisa menggunakan nilai default seperti 'no_image'
+    }
 
-      const attendanceId = result.insertId;
+    db.query(
+      "INSERT INTO attendance (check_in_time, check_out_time,  userId, attendance_category_id, attendance_status_id, attendance_date, upload_image) VALUES (NOW(), NULL, ?, ?, ?, CURDATE(), ?)",
+      [userId, attendance_category_id, attendance_status_id, upload_image],
+      (err, result) => {
+        if (err) {
+          console.error("Error during check-in:", err.message);
+          return res.status(500).json({ message: "Failed to check in" });
+        }
 
-      const queryAttendanceDetails = `
+        const attendanceId = result.insertId;
+
+        const queryAttendanceDetails = `
         SELECT a.attendance_date, s.attendance_status AS attendance_status
         FROM attendance a
         JOIN attendance_status s ON a.attendance_status_id = s.attendance_status_id
         WHERE a.attendanceId = ?
       `;
 
-      db.query(queryAttendanceDetails, [attendanceId], (err, detailsResult) => {
-        if (err) {
-          console.error("Error retrieving attendance details:", err.message);
-          return res
-            .status(500)
-            .json({ message: "Failed to retrieve attendance details" });
-        }
+        db.query(
+          queryAttendanceDetails,
+          [attendanceId],
+          (err, detailsResult) => {
+            if (err) {
+              console.error(
+                "Error retrieving attendance details:",
+                err.message
+              );
+              return res
+                .status(500)
+                .json({ message: "Failed to retrieve attendance details" });
+            }
 
-        res.status(200).json({
-          attendanceId,
-          attendance_status: detailsResult[0].attendance_status,
-        });
-      });
-    }
-  );
-});
+            res.status(200).json({
+              attendanceId,
+              attendance_status: detailsResult[0].attendance_status,
+            });
+          }
+        );
+      }
+    );
+  }
+);
 
 // Endpoint untuk Check-Out
 app.post("/attendance/checkout", verifyToken, (req, res) => {
