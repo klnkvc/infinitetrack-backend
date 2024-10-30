209ce925771b585ccc048765455b096b21fb7796
ririsutri <sutrikuliah@gmail.com>
2024-10-30 16:26:36 +0700

    Updated Attendance Logic

--- Controllers/attendance_Controller.js
+++ Controllers/attendance_Controller.js
@@ -1,9 +1,8 @@
-// controllers/attendance_Controller.js
 const { infinite_track_connection: db } = require("../dbconfig");
 const { haversineDistance } = require("../utils/geofence");
 
 const getAttendanceCategoryId = (category) => {
-  return category === "Work From Office" ? 1 : 2; // 1 untuk WFH, 2 untuk WFO
+  return category === "Work From Office" ? 1 : 2; // 1 untuk WFO, 2 untuk WFH
 };
 
 // Fungsi untuk mendapatkan ID status absensi
@@ -15,138 +14,125 @@ const getAttendanceStatusId = (status) => {
 const officeLocation = {
   latitude: 1.1853258302684722,
   longitude: 104.10194910214162,
-}; // Infinite Learning, Batam
+};
 
-const checkIn = (req, res) => {
-  const { attendance_category } = req.body;
-  let { latitude, longitude } = req.body; // Menggunakan 'let' agar nilai dapat diubah
+const handleAttendance = (req, res) => {
+  const { attendance_category, action } = req.body;
+  let { latitude, longitude } = req.body;
   const attendance_category_id = getAttendanceCategoryId(attendance_category);
   const userId = req.user.id;
 
   let upload_image = null;
+  const now = new Date();
+  const currentHour = now.getHours();
+  let attendance_status_id;
 
-  // Jika kategori adalah Work From Home (ID 2), validasi latitude/longitude diabaikan
-  if (attendance_category_id === 2) {
-    // Validasi untuk Work From Home
-    if (!req.file) {
-      return res
-        .status(400)
-        .json({ message: "Image is required for Work From Home" });
+  if (action === "checkin") {
+    // Handle Check-In Logic
+    if (attendance_category_id === 2) {
+      // Validasi untuk Work From Home (WFO)
+      if (!req.file) {
+        return res
+          .status(400)
+          .json({ message: "Image is required for Work From Home" });
+      }
+      upload_image = req.file.path;
+      latitude = null;
+      longitude = null;
+    } else {
+      // Validasi geofence (radius dalam meter)
+      const allowedRadius = 125; // 125 meter
+      const userLocation = {
+        latitude: parseFloat(latitude),
+        longitude: parseFloat(longitude),
+      };
+
+      const distance = haversineDistance(officeLocation, userLocation);
+      if (distance > allowedRadius) {
+        return res
+          .status(400)
+          .json({ message: "Location out of allowed radius" });
+      }
+      upload_image = "";
     }
-    upload_image = req.file.path;
 
-    // Tidak memerlukan validasi lokasi
-    latitude = null;
-    longitude = null;
+    attendance_status_id = currentHour < 9 ? 1 : 2; // Check for lateness
+
+    db.query(
+      "INSERT INTO attendance (check_in_time, check_out_time, userId, attendance_category_id, attendance_status_id, attendance_date, latitude, longitude, upload_image) VALUES (NOW(), NULL, ?, ?, ?, CURDATE(), ?, ?, ?)",
+      [
+        userId,
+        attendance_category_id,
+        attendance_status_id,
+        latitude,
+        longitude,
+        upload_image,
+      ],
+      (err, result) => {
+        if (err) {
+          console.error("Error during check-in:", err.message);
+          return res.status(500).json({ message: "Failed to check in" });
+        }
+
+        const attendanceId = result.insertId;
+
+        const queryAttendanceDetails = `
+          SELECT a.attendance_date, s.attendance_status AS attendance_status
+          FROM attendance a
+          JOIN attendance_status s ON a.attendance_status_id = s.attendance_status_id
+          WHERE a.attendanceId = ?
+        `;
+
+        db.query(
+          queryAttendanceDetails,
+          [attendanceId],
+          (err, detailsResult) => {
+            if (err) {
+              console.error(
+                "Error retrieving attendance details:",
+                err.message
+              );
+              return res
+                .status(500)
+                .json({ message: "Failed to retrieve attendance details" });
+            }
+
+            res.status(200).json({
+              message: "Check-in successful",
+              attendanceId,
+              attendance_status: detailsResult[0].attendance_status,
+            });
+          }
+        );
+      }
+    );
+  } else if (action === "checkout") {
+    // Handle Check-Out Logic
+    attendance_status_id = currentHour > 17 ? 3 : 1; // 3 untuk overtime, 1 untuk normal
+
+    db.query(
+      "UPDATE attendance SET check_out_time = NOW(), attendance_status_id = ? WHERE userId = ? AND attendance_date = CURDATE() AND check_out_time IS NULL",
+      [attendance_status_id, userId],
+      (err, result) => {
+        if (err) {
+          console.error("Error during check-out:", err.message);
+          return res.status(500).json({ message: "Failed to check out" });
+        }
+
+        if (result.affectedRows === 0) {
+          return res
+            .status(400)
+            .json({ message: "No active check-in found for today" });
+        }
+
+        res.status(200).json({ message: "Check-out successful" });
+      }
+    );
   } else {
-    // Validasi geofence (radius dalam meter)
-    const allowedRadius = 125; // 125 meter
-    const userLocation = {
-      latitude: parseFloat(latitude),
-      longitude: parseFloat(longitude),
-    };
-
-    const distance = haversineDistance(officeLocation, userLocation);
-    if (distance > allowedRadius) {
-      return res
-        .status(400)
-        .json({ message: "Location out of allowed radius" });
-    }
-    upload_image = "";
+    res.status(400).json({ message: "Invalid action" });
   }
-
-  const now = new Date();
-  const currentHour = now.getHours();
-  let attendance_status_id = currentHour < 9 ? 1 : 2;
-
-  db.query(
-    "INSERT INTO attendance (check_in_time, check_out_time, userId, attendance_category_id, attendance_status_id, attendance_date, latitude, longitude, upload_image) VALUES (NOW(), NULL, ?, ?, ?, CURDATE(), ?, ?, ?)",
-    [
-      userId,
-      attendance_category_id,
-      attendance_status_id,
-      latitude,
-      longitude,
-      upload_image,
-    ],
-    (err, result) => {
-      if (err) {
-        console.error("Error during check-in:", err.message);
-        return res.status(500).json({ message: "Failed to check in" });
-      }
-
-      const attendanceId = result.insertId;
-
-      const queryAttendanceDetails = `
-        SELECT a.attendance_date, s.attendance_status AS attendance_status
-        FROM attendance a
-        JOIN attendance_status s ON a.attendance_status_id = s.attendance_status_id
-        WHERE a.attendanceId = ?
-      `;
-
-      db.query(queryAttendanceDetails, [attendanceId], (err, detailsResult) => {
-        if (err) {
-          console.error("Error retrieving attendance details:", err.message);
-          return res
-            .status(500)
-            .json({ message: "Failed to retrieve attendance details" });
-        }
-
-        res.status(200).json({
-          attendanceId,
-          attendance_status: detailsResult[0].attendance_status,
-        });
-      });
-    }
-  );
-};
-
-const checkOut = (req, res) => {
-  const { attendance_category } = req.body;
-  const attendance_category_id = getAttendanceCategoryId(attendance_category);
-  const userId = req.user.id;
-
-  const now = new Date();
-  const currentHour = now.getHours();
-
-  let attendance_status_id = currentHour > 17 ? 3 : 1;
-
-  db.query(
-    "INSERT INTO attendance (check_in_time, check_out_time, userId, attendance_category_id, attendance_status_id, attendance_date) VALUES (NULL, NOW(), ?, ?, ?, CURDATE())",
-    [userId, attendance_category_id, attendance_status_id],
-    (err, result) => {
-      if (err) {
-        console.error("Error during check-out:", err.message);
-        return res.status(500).json({ message: "Failed to check out" });
-      }
-
-      const attendanceId = result.insertId;
-
-      const queryAttendanceDetails = `
-        SELECT a.attendance_date, s.attendance_status AS attendance_status
-        FROM attendance a
-        JOIN attendance_status s ON a.attendance_status_id = s.attendance_status_id
-        WHERE a.attendanceId = ?
-      `;
-
-      db.query(queryAttendanceDetails, [attendanceId], (err, detailsResult) => {
-        if (err) {
-          console.error("Error retrieving attendance details:", err.message);
-          return res
-            .status(500)
-            .json({ message: "Failed to retrieve attendance details" });
-        }
-
-        res.status(200).json({
-          attendanceId,
-          attendance_status: detailsResult[0].attendance_status,
-        });
-      });
-    }
-  );
 };
 
 module.exports = {
-  checkIn,
-  checkOut,
+  handleAttendance,
 };
--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,19 +1,21 @@
-// routes/attendanceRoutes.js
 const express = require("express");
 const { verifyToken } = require("../middleware/authMiddleWare");
-const { checkIn, checkOut } = require("../Controllers/attendance_Controller");
+const { handleAttendance } = require("../Controllers/attendance_Controller");
 const multer = require("multer");
 const path = require("path");
 
-// Setup multer untuk file upload
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Memberi nama unik pada gambar
+    const today = new Date().toISOString().split("T")[0];
+
+    const formattedName = `${today}-${file.originalname}`;
+    cb(null, formattedName);
   },
 });
+
 const upload = multer({
   storage: storage,
   limits: { fileSize: 1024 * 1024 * 5 }, // Batas ukuran file 5MB
@@ -21,10 +23,12 @@ const upload = multer({
 
 const router = express.Router();
 
-// Route untuk Check-In
-router.post("/checkin", upload.single("upload_image"), checkIn);
-
-// Route untuk Check-Out
-router.post("/checkout", checkOut);
+// Route untuk Check-In dan Check-Out
+router.post(
+  "/users",
+  verifyToken,
+  upload.single("upload_image"),
+  handleAttendance
+);
 
 module.exports = router;
