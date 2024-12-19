399ee275fe69fa9e6f04be71a1d2fb4f4867350a
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2024-12-20 00:28:09 +0700

    Reverted Date Response

--- Controllers/leave_Controller.js
+++ Controllers/leave_Controller.js
@@ -445,31 +445,27 @@ function getAllLeaveUsers(req, res) {
 
     const formatDateTime = (dateTime) => {
       const date = new Date(dateTime);
-      const yyyy = date.getFullYear();
-      const mm = String(date.getMonth() + 1).padStart(2, "0");
-      const dd = String(date.getDate()).padStart(2, "0");
-      return `${yyyy}-${mm}-${dd}`;
-      // const day = String(date.getDate()).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
 
-      // const months = [
-      //   "Januari",
-      //   "Februari",
-      //   "Maret",
-      //   "April",
-      //   "Mei",
-      //   "Juni",
-      //   "Juli",
-      //   "Agustus",
-      //   "September",
-      //   "Oktober",
-      //   "November",
-      //   "Desember",
-      // ];
+      const months = [
+        "Januari",
+        "Februari",
+        "Maret",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Agustus",
+        "September",
+        "Oktober",
+        "November",
+        "Desember",
+      ];
 
-      // const month = months[date.getMonth()];
-      // const year = date.getFullYear();
+      const month = months[date.getMonth()];
+      const year = date.getFullYear();
 
-      // return `${day} ${month} ${year}`;
+      return `${day} ${month} ${year}`;
     };
 
     const formatTimeAgo = (submittedAt) => {
